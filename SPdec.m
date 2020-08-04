72a4f54680839b5e63726852954d226fe51cfb5f
Zhang Hang <59041932+FattyZH@users.noreply.github.com>
2020-08-04 15:12:58 +0800

    Update SPdec.m

--- SPdec.m
+++ SPdec.m
@@ -1,41 +1,37 @@
-function[Stream,Potential] = SPdec(u,v)
-
+function[Stream,Potential] = SPdec(u,v,dd)
+% dd is interval of mesh
+if nargin == 2
+    dd = 1;
+end
+%% deal with NAN
 unan = isnan(u);
 vnan = isnan(v);
 u(unan) = 0;
 v(vnan) = 0;
-
-div = divergence(u,v);
+%% calculate Potential function with divergence field and 0 Dirichlet condition
+div = divergence(u,v)/dd;
 Potential = zeros(size(div)+2);
-Potential(2:end-1,2:end-1) = poisson_dst(div);
-[uk,vk] = gradient(Potential);
+Potential(2:end-1,2:end-1) = poisson_dst(div,dd,dd);
+
+%% Remove Potential flow field
+[uk,vk] = gradient(Potential/dd);
 u = u-uk(2:end-1,2:end-1);
 v = v-vk(2:end-1,2:end-1);
 
-
-ul = -(u(1:end-1,1)+u(2:end,1))/2;ur = (u(1:end-1,end)+u(2:end,end))/2;
-vb = -(v(1,1:end-1)+v(1,2:end))/2;vu = (v(end,1:end-1)+v(end,2:end))/2;
-vm = mean([ul(:);ur(:);vb(:);vu(:)]);
-ul = ul-vm; ur = ur-vm; vb = vb-vm;vu = vu-vm;
+%% calculate Boundary conditions of Steam function and curl field
 cur = zeros(size(u));
-for i = 1:size(u,1)-1
-    cur(i+1,1) = cur(i,1)-ul(i);
-end
-for i = 1:size(u,2)-1
-    cur(1,i+1) = cur(1,i)+vb(i);
-end
-for i = 1:size(u,1)-1
-    cur(i+1,end) = cur(i,end)+ur(i);
-end
-for i = 1:size(u,2)-2
-    cur(end,i+1) = cur(end,i)-vu(i);
-end
-cur(1,1) = 2*cur(1,1);cur(1,end) = 2*cur(1,end);cur(end,1) = 2*cur(end,1);cur(end,end) = 2*cur(end,end);
-cur = -cur - 2*curl(u,v);
+cur(1,:) = cur(1,:) + cur(1,1) + cumtrapz(-v(1,:));
+cur(:,1) = cur(:,1) + cur(1,1) + cumtrapz(u(:,1));
+cur(end,:) = cur(end,:) + cur(end,1) + cumtrapz(-v(end,:));
+cur(:,end) = cur(:,end) + cur(1,end) + cumtrapz(u(:,end));
 
-Stream = poisson_dst(cur);
-Potential = Potential(2:end-1,2:end-1);
+cur = (-cur - 2*curl(u,v))/dd;
 
+%% In Physical, we are used to Counter sign
+Stream = -poisson_dst(cur,dd,dd);
+Potential = -Potential(2:end-1,2:end-1);
+
+%% deal with NAN
 Stream(unan&vnan) = nan;
 Potential(unan&vnan) = nan;
 end
